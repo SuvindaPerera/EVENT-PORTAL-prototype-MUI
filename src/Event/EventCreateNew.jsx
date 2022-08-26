3e1f51f51221f3d0a3c5607bda597087928aa0f7
Suvinda <eraiensuvinda00304@gmail.com>
2022-08-26 22:19:47 +0530

    CreateEvent page complete

--- src/Event/EventCreateNew.jsx
+++ src/Event/EventCreateNew.jsx
@@ -1,4 +1,5 @@
-import { Box, TextField, Typography } from '@mui/material'
+import * as React from 'react';
+import { Box, FormGroup, TextField, Typography } from '@mui/material'
 import UserNavbar from '../components/UserNavbar'
 import Grid from '@mui/material/Unstable_Grid2';
 // import { DayPicker } from 'react-day-picker';
@@ -6,6 +7,13 @@ import 'react-datepicker/dist/react-datepicker.css';
 import DatePicker from 'react-datepicker';
 import { useState } from 'react';
 import 'react-datepicker/dist/react-datepicker.css';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Button from '@mui/material/Button';
 // import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 // import LocalizationProvider from '@mui/x-date-pickers/LocalizationProvider';
 
@@ -15,18 +23,23 @@ import 'react-datepicker/dist/react-datepicker.css';
 
 const EventCreateNew = () => {
 
+//Handle dates
 const [selectdStartDate, setSelectedStartDate] = useState(null)
 const [selectdEndDate, setSelectedEndDate] = useState(null)
+
+//handle Price Negotiable Tick
+const [checked, setChecked] = React.useState(true);
+  const handleChange = (event) => {
+    setChecked(event.target.checked);
+  };
 
   return (
     <Box>
         <UserNavbar/>
-        <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        <Box sx={{ display: 'flex', justifyContent: 'center' }}>
             <Grid container 
             spacing={2} 
             mt={2} 
-            justifyContent="center"
-            alignItems="center"
             maxWidth="sm"
             >
                 <Grid item xs={12} md={12}>
@@ -47,11 +60,24 @@ const [selectdEndDate, setSelectedEndDate] = useState(null)
                 <Grid item xs={12} md={12}>
                     <Grid container 
                         spacing={2}  
-                        justifyContent="center"
-                        alignItems="center"
+                        justifyContent="left"
                         >
+                        
                         <Grid item xs={12} md={12}>
                             <h3>Select Date/Dates</h3>
+                        </Grid>
+                        <Grid item xs={12} md={12}>
+                            <FormControl>
+                            <FormLabel id="selectDate">Duration of the event</FormLabel>
+                            <RadioGroup
+                                row
+                                aria-labelledby="demo-row-radio-buttons-group-label"
+                                name="row-radio-buttons-group"
+                            >
+                                <FormControlLabel value="oneDay" control={<Radio />} label="One Day Event" />
+                                <FormControlLabel value="moreDays" control={<Radio />} label="Not a single day event" />
+                            </RadioGroup>
+                            </FormControl>
                         </Grid>
                         <Grid item xs={12} md={6}>
                             
@@ -77,6 +103,45 @@ const [selectdEndDate, setSelectedEndDate] = useState(null)
                             showYearDropdown
                             scrollableMonthYearDropdown
                             />
+                        </Grid>
+                        <Grid item xs={12} md={12}>
+                            <h3>Your Budget</h3>
+                        </Grid>
+                        <Grid Item xs={12} md={3}>
+                            <FormGroup aria-label="position" row>
+                                <FormControlLabel 
+                                control={<Checkbox />} 
+                                label="Negotiable"
+                                labelPlacement="start"
+                                />
+                            </FormGroup>
+                        </Grid>
+                        <Grid item xs={12} md={9}>
+                            <TextField 
+                            fullWidth label="Your budget in LKR" 
+                            variant="outlined"
+                            type="number" 
+                            maxValue="10"
+                            />    
+                        </Grid>
+                        <Grid item xs={12} md={6}>
+                            <Button 
+                            variant="outlined"
+                            fullWidth
+                            sx={{  borderRadius:"50px" }}
+                            >
+                            Clear All
+                            </Button>
+                        </Grid>
+                        <Grid item xs={12} md={6}>
+                            <Button 
+                            variant="contained"
+                            fullWidth
+                            type="submit"
+                            sx={{  borderRadius:"50px" }}
+                            >
+                            Create New Event
+                            </Button>
                         </Grid>
                     </Grid>
                 </Grid>

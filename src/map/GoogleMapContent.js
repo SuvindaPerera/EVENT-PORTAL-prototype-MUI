22623a7eb36e0336ce05a79b504e58ec231351fd
Suvinda Perera <48805257+SuvindaPerera@users.noreply.github.com>
2022-09-15 09:22:41 +0530

    Map Search added

---
+++ src/map/GoogleMapContent.js
@@ -0,0 +1,121 @@
+import {GoogleApiWrapper,Map, InfoWindow, Marker} from 'google-maps-react';
+import React, { Component } from 'react';
+import PlacesAutocomplete, {
+  geocodeByAddress,
+  getLatLng,
+} from 'react-places-autocomplete';
+import Box from '@mui/material/Box';
+
+
+export class MapContainer extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { 
+      address: '' ,
+      showingInfoWindow: false,
+      activeMarker: {},
+      selectedPlace: {},
+
+      mapCenter:{
+        lat: 7.8731,
+        lng: 80.7718
+      }
+    
+    };
+  }
+
+    
+   
+    handleChange = address => {
+      this.setState({ address });
+    };
+   
+    handleSelect = address => {
+      geocodeByAddress(address)
+        .then(results => getLatLng(results[0]))
+        .then(latLng => {
+          console.log('Success', latLng);
+          this.setState({address});
+          this.setState({mapCenter:latLng});
+          })
+        .catch(error => console.error('Error', error));
+    };
+   
+    render() {
+      return (
+        <Box
+        sx={{
+          height: '100',
+          width: '100',
+        }}
+
+        >
+          <PlacesAutocomplete
+            value={this.state.address}
+            onChange={this.handleChange}
+            onSelect={this.handleSelect}
+          >
+            {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+              <div>
+                <input size = "50" 
+                  {...getInputProps({
+                    placeholder: 'Search Places ...',
+                    className: 'location-search-input',
+                  })}
+                />
+                <div className="autocomplete-dropdown-container">
+                  {loading && <div>Loading...</div>}
+                  {suggestions.map(suggestion => {
+                    const className = suggestion.active
+                      ? 'suggestion-item--active'
+                      : 'suggestion-item';
+
+                    const style = suggestion.active
+                      ? { backgroundColor: '#fafafa', cursor: 'pointer' }
+                      : { backgroundColor: '#ffffff', cursor: 'pointer' };
+                    return (
+                      <div
+                        {...getSuggestionItemProps(suggestion, {
+                          className,
+                          style,
+                        })}
+                      >
+                        <span>{suggestion.description}</span>
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+            )}
+          </PlacesAutocomplete>
+
+          <div className="map">
+            <Map 
+            google={this.props.google}
+            initialCenter={{
+              lat: this.state.mapCenter.lat,
+              lng: this.state.mapCenter.lng
+            }}
+            center={{
+              lat: this.state.mapCenter.lat,
+              lng: this.state.mapCenter.lng
+            }}
+            zoom={9}
+            >
+              <Marker 
+                position={{
+                  lat: this.state.mapCenter.lat,
+                  lng: this.state.mapCenter.lng
+                }}
+              />
+            </Map>
+          </div>
+        </Box>
+      )
+    }
+  }
+
+  export default GoogleApiWrapper({
+    apiKey: (process.env.REACT_APP_GOOGLE_MAPS_API_KEY)
+  })(MapContainer)
--- src/map/Map.jsx
+++ src/map/Map.jsx
@@ -3,21 +3,23 @@ import { useJsApiLoader, GoogleMap} from '@react-google-maps/api'
 import Skeleton from '@mui/material/Skeleton';
 import Box from '@mui/material/Box';
 import LandingPageNavbar from '../components/LandingPageNavbar'
+import GoogleMapContent from './GoogleMapContent'
+import { Grid } from '@mui/material';
 
 
 
-const center = { lat: 7.8731, lng: 80.7718}
+// const center = { lat: 7.8731, lng: 80.7718}
 
 
 
 const Map = () => {
 
-    const {isLoaded}= useJsApiLoader({
-        googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY
-    })
-    if(!isLoaded){
-        return <Skeleton variant="rounded" width={210} height={60} />
-    }
+//     const {isLoaded}= useJsApiLoader({
+//         googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY
+//     })
+//     if(!isLoaded){
+//         return <Skeleton variant="rounded" width={210} height={60} />
+//     }
 
 
   return (
@@ -28,17 +30,22 @@ const Map = () => {
       alignItems: 'center',
       justifyContent: 'center',
       backgroundColor: '#f5f5f5',
-      display: 'flex',
-      flexDirection: 'column',
       }}>
         <LandingPageNavbar />
-        <h3 className="mapTitle">Search the Map</h3>
-        <GoogleMap 
-        center={center} 
-        zoom={8} 
-        mapContainerStyle={{width: '100%', height: '100%'}}
-        >
-        </GoogleMap>
+        <Grid container 
+         sx={{
+          paddingTop: '1vh',
+          alignItems: 'center',
+          justifyContent: 'center',
+          
+          }}>
+          <Grid item sm={12} md={12}>
+            <h3 className="mapTitle">Search the Map</h3>
+          </Grid>
+          <Grid item sm={12} md={12}>
+            <GoogleMapContent />
+          </Grid>
+        </Grid>
     </Box>
   )
 }
